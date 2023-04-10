096bda021bfd5fd9a4e8eee843aa6481b4742702
RuslanDidun <ruslandidun5@gmail.com>
2023-04-10 16:46:23 -0600

    add input component

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -6,6 +6,8 @@ import { useCallback, useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import useRegisterModal from "@/app/hooks/useRegisterModal";
 import Modal from './Modal';
+import Heading from '../Heading';
+import Input from '../inputs/Input';
 
 
 const RegisterModal = () => {
@@ -38,6 +40,42 @@ const RegisterModal = () => {
     });
   }
 
+  const bodyContent = (
+    <div className='flex flex-col gap-4'>
+      <Heading
+        title='Welcome to Airbnb'
+        subtitle='Create an account'
+      />
+      <Input
+        id='email'
+        label='Email'
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+
+      <Input
+        id='name'
+        label='Name'
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+
+      <Input
+        id='password'
+        type='password'
+        label='Password'
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+    </div>
+  )
+
   return (
     <Modal
       disabled={isLoading}
@@ -46,6 +84,7 @@ const RegisterModal = () => {
       actionLabel='Continue'
       onClose={registerModal.onClose}
       onSubmit={handleSubmit(onSubmit)}
+      body={bodyContent}
     />
   );
 };
--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -3,9 +3,11 @@ import { useState, useCallback } from 'react';
 import { AiOutlineMenu } from 'react-icons/ai';
 import Avatar from '../Avatar';
 import MenuItem from './MenuItem';
+import useRegisterModal from '@/app/hooks/useRegisterModal';
 
 const UserMenu = () => {
 
+  const registerModal = useRegisterModal();
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleOpen = useCallback(() => {
@@ -37,7 +39,7 @@ const UserMenu = () => {
               <MenuItem onClick={() => { }}
                 label="Login"
               />
-              <MenuItem onClick={() => { }}
+              <MenuItem onClick={registerModal.onOpen}
                 label="Sign Up"
               />
             </>
