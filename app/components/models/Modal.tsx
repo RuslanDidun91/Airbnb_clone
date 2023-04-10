64040e1602462fa7ef2adfaafccb4b1e81230e3d
RuslanDidun <ruslandidun5@gmail.com>
2023-04-10 09:52:06 -0600

    auth Modal

--- app/components/models/Modal.tsx
+++ app/components/models/Modal.tsx
@@ -1,13 +1,87 @@
+'use client';
+import { useState, useEffect, useCallback } from 'react';
+import { IoMdClose } from 'react-icons/io';
 
+interface ModalProps {
+  isOpen?: boolean;
+  onClose: () => void;
+  onSubmit: () => void;
+  title?: string;
+  body?: string;
+  footer?: React.ReactElement;
+  actionLabel: string;
+  disabled?: string;
+  secondaryAction?: () => void;
+  secondaryLabel?: string;
+}
 
-const Modal = () => {
+const Modal: React.FC<ModalProps> = ({
+  isOpen, onClose, onSubmit, title, body, footer, actionLabel,
+  disabled, secondaryAction, secondaryLabel
+}) => {
 
+  const [showModal, setShowModal] = useState(isOpen);
 
-  
+  useEffect(() => {
+    setShowModal(isOpen);
+  }, [isOpen]);
+
+  //in order to add some animation for 300ms
+  const handleClose = useCallback(() => {
+    if (disabled) {
+      return;
+    }
+    setShowModal(false);
+    setTimeout(() => {
+      onClose();
+    }, 300)
+  }, [disabled, onClose]);
+
+  const handleSubmit = useCallback(() => {
+    if (disabled) {
+      return;
+    }
+    onSubmit();
+  }, [disabled, onSubmit]);
+
+  const handleSecondaryAction = useCallback(() => {
+    if (disabled || !secondaryAction) {
+      return;
+    }
+    secondaryAction();
+  }, [disabled, secondaryAction]);
+
+  if (!isOpen) {
+    return null;
+  }
+
   return (
-    <div>
-
-    </div>
+    <>
+      <div className='justify-center items-center flex overflow-x-hidden overflow-y-auto
+          fixed inset-0 z-50 outline-none focus:outline-none bg-neutral-800/70'>
+        <div className='relative w-full md:w-4/6 lg:w-3/6 xl:w-2/5 my-6 mx-auto 
+            h-full lg:h-auto md:h-auto'>
+          {/* Content */}
+          <div className={`translate duration-300 h-full 
+          ${showModal ? 'translate-y-0' : 'translate-y-full'}
+          ${showModal ? 'opacity-100' : 'opacity-0'}`
+          }>
+            <div className='translate h-full lg:h-auto md:h-auto border-0 rounded-lg
+            shadow-lg relative flex flex-col w-full bg-white outline-none focul:outline-none'>
+              {/* Header */}
+              <div className='flex items-center p-6 rounded-t justify-center relative
+                   border-b-[1px]'>
+                <button onClick={onClose}
+                  className='p-1 border-0 hover:opacity-70 transition absolute left-9'
+                >
+                  <IoMdClose size={18} />
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
