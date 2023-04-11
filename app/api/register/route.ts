1a7afc73331a17d12180441ec11a5cf631c343e3
RuslanDidun <ruslandidun5@gmail.com>
2023-04-11 11:32:39 -0600

    add register routes

---
+++ app/api/register/route.ts
@@ -0,0 +1,17 @@
+import { NextResponse } from "next/server";
+import bcrypt from "bcrypt";
+import prisma from "@/app/libs/prismadb";
+
+
+export async function POST(
+  request: Request
+) {
+  const body = await request.json();
+  const { email, name, password } = body;
+  //wait until bcrypt hash the pasword
+  const hashedPassword = await bcrypt.hash(password, 12);
+  //creating new user
+  const user = await prisma.user.create({ data: { email, name, hashedPassword } });
+
+  return NextResponse.json(user);
+}
