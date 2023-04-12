bf5bd8b2a4635b5db57667d66285c3960011d186
RuslanDidun <ruslandidun5@gmail.com>
2023-04-12 10:03:09 -0600

    fix bug

--- app/components/CategoryBox.tsx
+++ app/components/CategoryBox.tsx
@@ -1,3 +1,4 @@
+'use client'
 import { IconType } from "react-icons";
 import { useRouter, useSearchParams } from "next/navigation";
 import { useCallback } from "react";
--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -8,6 +8,7 @@ const Logo = () => {
 
   return (
     <Image alt='Logo'
+      onClick={() => router.push('/')}
       className='hidden md:block cursor-pointer'
       height='100'
       width='100'
