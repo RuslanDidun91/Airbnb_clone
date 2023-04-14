2da8f908b020335dd6753e49cfc04d683ee8c744
RuslanDidun <ruslandidun5@gmail.com>
2023-04-14 13:01:53 -0600

    add listingHead & listingInfo components

--- app/components/listings/ListingHead.tsx
+++ app/components/listings/ListingHead.tsx
@@ -1,5 +1,10 @@
-
+'use client'
+import Image from "next/image";
 import { SafeUser } from "@/app/types";
+import useCountries from "@/app/hooks/useCountries";
+import Heading from "../Heading";
+import HeartButton from "../HeartButton";
+
 
 interface ListingHeadProps {
   title: string;
@@ -9,11 +14,34 @@ interface ListingHeadProps {
   currentUser?: SafeUser | null
 }
 
-const ListingHead: React.FC<ListingHeadProps> = ({}) => {
-  return(
-    <div>
+const ListingHead: React.FC<ListingHeadProps> = ({
+  title, locationValue, imageSrc, id, currentUser
+}) => {
 
-    </div>
+  const { getByValue } = useCountries();
+  const location = getByValue(locationValue);
+
+  return (
+    <>
+      <Heading
+        title={title}
+        subtitle={`${location?.region}, ${location?.label}`}
+      />
+      <div className="w-full h-[60vh] overflow-hidden rounded-xl relative">
+        <Image
+          src={imageSrc}
+          fill
+          className="object-cover w-full"
+          alt="Image"
+        />
+        <div className="absolute top-5 right-5">
+          <HeartButton
+            listingId={id}
+            currentUser={currentUser}
+          />
+        </div>
+      </div>
+    </>
   );
 };
 
--- app/listings/[listingId]/ListingClient.tsx
+++ app/listings/[listingId]/ListingClient.tsx
@@ -1,9 +1,11 @@
+'use client'
 import { Reservation } from "@prisma/client";
 import { SafeListing, SafeUser } from "@/app/types";
 import { useMemo } from "react";
 import { categories } from "@/app/components/navbar/Categories";
 import Container from "@/app/components/Container";
 import ListingHead from "@/app/components/listings/ListingHead";
+import ListingInfo from "@/app/components/listings/ListingInfo";
 
 interface ListingClientProps {
   reservations?: Reservation[];
@@ -19,7 +21,7 @@ const ListingClient: React.FC<ListingClientProps> = ({ listing, currentUser }) =
 
   return (
     <Container>
-      <div className="max-w-screen-lg mx-auto">       
+      <div className="max-w-screen-lg mx-auto">
         <div className="flex flex-col gap-6">
           <ListingHead
             title={listing.title}
@@ -28,6 +30,17 @@ const ListingClient: React.FC<ListingClientProps> = ({ listing, currentUser }) =
             id={listing.id}
             currentUser={currentUser}
           />
+          <div className="grid grid-cols-1 md:grid-cols-7 md:gap-10 mt-6">
+            <ListingInfo
+              user={listing.user}
+              category={category}
+              description={listing.description}
+              roomCount={listing.roomCount}
+              guestCount={listing.guestCount}
+              bathroomCount={listing.bathroomCount}
+              locationValue={listing.locationValue}
+            />
+          </div>
         </div>
       </div>
     </Container>
