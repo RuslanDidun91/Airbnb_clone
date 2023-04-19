d3dc77fa23c32c87b6d9c314c9fc4e918b5298a5
RuslanDidun <ruslandidun5@gmail.com>
2023-04-18 19:33:07 -0600

    add to my favorites

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -63,7 +63,7 @@ const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
                 />
                 <MenuItem
                   label="My favorites"
-                  onClick={() => { }}
+                  onClick={() => router.push('/favorites')}
                 />
                 <MenuItem
                   label="My reservations"
